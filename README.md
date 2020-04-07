e65d775f3fb340db97dede83611af2874fc25659
Tyler Bates <Fondlytyler@gmail.com>
2020-04-07 14:57:43 -0700

    create object for user input and applied  prototype method to calculate price of ticket.

---
+++ README.md
@@ -0,0 +1,23 @@
+###Spec - Ensure each input has a selection
+####input: no 'movie time' or 'age' selected
+####output: "please select a movie time/age"
+
+###Spec - send user input to Ticket Constructor
+####input: - movie title, movie time, user age
+####output: - Ticket values
+
+###Spec - Ticket Constructor sends values to MovieTickets function
+####input: ticket values
+####output: ticket object
+
+###Spec - Ticket price determined
+####input: ticket object
+####output: ticket price based on ticket values
+
+###Spec - Ticket price is displayed to user
+####input: - ticket price
+####output: - ticket price displayed on page
+
+###Spec - Get input on amount of tickets desired
+####input: number of tickets
+####output: checkout price
---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+//Business Logic:
+// function PriceSummary() {
+//   this.tickets = [];
+// }
+
+
+function Ticket(title, time, age) {
+  this.title = title;
+  this.time = time;
+  this.age = age; 
+}
+
+// Ticket.prototype.summary = function() {
+//  $(placeholder)return this.title + " at " + this.time;
+// }
+
+// write a prototype method that uses the properties of the object to determine the price and adds price as a new property to the object
+
+Ticket.prototype.calculatePrice = function() {
+  var price = 8;
+  if (this.time === cheap || this.age < 10 || this.age > 65){
+    price -= 2
+  } else if (this.title == 'Fast and Furious....innnnnnn Spaaaaaaace (New Release)') {
+      price += 10
+    }
+    else {
+      return price
+    }
+  }
+
+
+//User Interface Logic:
+
+$(document).ready(function() {
+  $("form#box-office").submit(function(event){
+    event.preventDefault();
+    var inputtedMovieTitle = $("#movie-title").val();
+    var inputtedMovieTime = $("input:radio[name=movie-time]:checked").val();
+    var userAge = parseInt($("input#age").val());
+    // if (isNaN(userAge)){
+      // $("output").append("Please enter your age using a number.")
+      // } else {
+        // }
+        
+    var ticket = new Ticket(inputtedMovieTitle, inputtedMovieTime, userAge);
+    // ticket.calculatePrice()
+    $("whatever").html(balloon.price)
+  })
+});
