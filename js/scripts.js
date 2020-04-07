cadd4bd81ff14e9257a24560b6a715cbfafb2134
Tyler Bates <Fondlytyler@gmail.com>
2020-04-07 16:10:35 -0700

    add return functionality to user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,25 @@
 //Business Logic:
-// function PriceSummary() {
-//   this.tickets = [];
-// }
 
 
 function Ticket(title, time, age) {
   this.title = title;
   this.time = time;
   this.age = age; 
+  // this.price = 8;
 }
-
-// Ticket.prototype.summary = function() {
-//  $(placeholder)return this.title + " at " + this.time;
-// }
 
 // write a prototype method that uses the properties of the object to determine the price and adds price as a new property to the object
 
 Ticket.prototype.calculatePrice = function() {
-  var price = 8;
-  if (this.time === cheap || this.age < 10 || this.age > 65){
-    price -= 2
-  } else if (this.title == 'Fast and Furious....innnnnnn Spaaaaaaace (New Release)') {
-      price += 10
+  this.price = 8;
+
+  if (this.time === "matinee" || this.age <= 10 || this.age >= 65){
+    this.price -= 2
+  } else (this.title == 'Fast and Furious....innnnnnn Spaaaaaaace (New Release)'); {
+      this.price += 10
     }
-    else {
-      return price
-    }
-    console.log(price)
+    
   }
-
-
 //User Interface Logic:
 
 $(document).ready(function() {
@@ -39,14 +29,16 @@ $(document).ready(function() {
     var inputtedMovieTime = $("input:radio[name=movie-time]:checked").val();
     var userAge = parseInt($("input#age").val());
     if (isNaN(userAge)){
-      $(".results").html("Please enter your age using a number.");
+      $(".results").html("Please REFRESH and enter your age using a number.");
       $("#showTime").hide();
       }
         
     var ticket = new Ticket(inputtedMovieTitle, inputtedMovieTime, userAge);
-    // ticket.calculatePrice(ticket)
-    $("priceOutput").html(ticket.price);
-    $(".results").fadeToggle();
+    ticket.calculatePrice();
+    $("#priceOutput").html(ticket.price);
     $("input#age").val("");
+    $("#showTime").text(ticket.title + " at " + ticket.time);
+    $(".results").fadeToggle();
+      console.log()
   })
 });
